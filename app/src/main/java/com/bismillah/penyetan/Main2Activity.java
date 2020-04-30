41d2f8a0d10cbfa14c7ec10e14c789c7548fe620
992510 <45156930+992510@users.noreply.github.com>
2020-04-30 17:17:50 +0700

    update mainActifity2

--- app/src/main/java/com/bismillah/penyetan/Main2Activity.java
+++ app/src/main/java/com/bismillah/penyetan/Main2Activity.java
@@ -3,6 +3,8 @@ package com.bismillah.penyetan;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,13 +12,53 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private TabItem katalog, hadiah;
+    public PageAdapter pageradapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        katalog = (TabItem) findViewById(R.id.Katalog);
+        hadiah = (TabItem) findViewById(R.id.Hadiah);
+        viewPager = findViewById(R.id.viewpager);
+
+        pageradapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageradapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() ==0 ){
+                    pageradapter.notifyDataSetChanged();
+                }
+                else if (tab.getPosition() ==1 ){
+                    pageradapter.notifyDataSetChanged();
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         TextView toolbarText = findViewById(R.id.textView_toolbar);
@@ -31,6 +73,7 @@ public class Main2Activity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
                 switch (menuItem.getItemId()) {
                     case R.id.beranda:
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
@@ -38,6 +81,8 @@ public class Main2Activity extends AppCompatActivity {
                         finish();
                         return true;
                     case R.id.hadiah:
+                        startActivity(new Intent(getApplicationContext(), Main2Activity.class));
+                        overridePendingTransition(0, 0);
                         finish();
                         return true;
                     case R.id.toko:
