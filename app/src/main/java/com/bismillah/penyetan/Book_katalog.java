bfc437a6692dafaccd36d90a23392c26fd1fe2c6
munawaroh04 <45156930+munawaroh04@users.noreply.github.com>
2020-04-30 18:42:58 +0700

    class book

---
+++ app/src/main/java/com/bismillah/penyetan/Book_katalog.java
@@ -0,0 +1,21 @@
+package com.bismillah.penyetan;
+
+class Book_katalog {
+
+    private int photo;
+
+    public Book_katalog() {
+    }
+
+    public Book_katalog(int photo) {
+        this.photo = photo;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+}
