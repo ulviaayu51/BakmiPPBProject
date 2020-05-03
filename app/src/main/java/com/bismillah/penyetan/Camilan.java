aadf2b7ff67142e90cb7bfd1d66f377924368601
ulviaayuc <47856128+ulviaayu51@users.noreply.github.com>
2020-05-03 21:11:36 +0700

    Update Camilan.java

--- app/src/main/java/com/bismillah/penyetan/Camilan.java
+++ app/src/main/java/com/bismillah/penyetan/Camilan.java
@@ -12,12 +12,8 @@ import android.widget.TextView;
 public class Camilan extends AppCompatActivity {
     ImageView imageView1;
     ImageView imageView2;
-    ImageView imageView3;
-    ImageView imageView4;
     TextView textView1;
     TextView textView2;
-    TextView textView3;
-    TextView textView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +23,15 @@ public class Camilan extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         TextView toolbarText = findViewById(R.id.textView_toolbar);
         if(toolbarText!=null && toolbar!=null){
-            toolbarText.setText("Camilan");
+            toolbarText.setText("Makanan Penutup & Camilan");
             setSupportActionBar(toolbar);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
         imageView1 = findViewById(R.id.imageView1_camilan);
         imageView2 = findViewById(R.id.imageView2_camilan);
-        imageView3 = findViewById(R.id.imageView3_camilan);
-        imageView4 = findViewById(R.id.imageView4_camilan);
         textView1 = findViewById(R.id.textView1_camilan);
         textView2 = findViewById(R.id.textView2_camilan);
-        textView3 = findViewById(R.id.textView3_camilan);
-        textView4 = findViewById(R.id.textView4_camilan);
 
         imageView1.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -51,22 +43,8 @@ public class Camilan extends AppCompatActivity {
         imageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(Camilan.this, DeskripsiCamilan2.class);
-                startActivity(intent2);
-            }
-        });
-        imageView3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent3 = new Intent(Camilan.this, DeskripsiCamilan3.class);
-                startActivity(intent3);
-            }
-        });
-        imageView4.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent4 = new Intent(Camilan.this, DeskripsiCamilan4.class);
-                startActivity(intent4);
+                Intent intent = new Intent(Camilan.this, DeskripsiCamilan2.class);
+                startActivity(intent);
             }
         });
     }
