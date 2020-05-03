04d775f14a7096a5e4c95d87c5fb2cc2d1d2f140
ulviaayuc <47856128+ulviaayu51@users.noreply.github.com>
2020-05-03 21:36:28 +0700

    Update MainActivity.java

--- app/src/main/java/com/bismillah/penyetan/MainActivity.java
+++ app/src/main/java/com/bismillah/penyetan/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,8 +20,17 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView3;
     ImageView imageView4;
     ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    ImageView imageView10;
+    ImageView imageView11;
+    ImageView imageView12;
     TextView textView1;
     TextView textView2;
+    Button button1;
+    Button button2;
 
 
     @Override
@@ -74,8 +84,17 @@ public class MainActivity extends AppCompatActivity {
         imageView3 = findViewById(R.id.imageView3_beranda);
         imageView4 = findViewById(R.id.imageView4_beranda);
         imageView5 = findViewById(R.id.imageView5_beranda);
+        imageView6 = findViewById(R.id.imageView6_beranda);
+        imageView7 = findViewById(R.id.imageView7_beranda);
+        imageView8 = findViewById(R.id.imageView8_beranda);
+        imageView9 = findViewById(R.id.imageView9_beranda);
+        imageView10 = findViewById(R.id.imageView10_beranda);
+        imageView11 = findViewById(R.id.imageView11_beranda);
+        imageView12 = findViewById(R.id.imageView12_beranda);
         textView1 = findViewById(R.id.textView1_beranda);
         textView2 = findViewById(R.id.textView2_beranda);
+        button1 = findViewById(R.id.button1_beranda);
+        button2 = findViewById(R.id.button2_beranda);
 
         imageView1.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -87,29 +106,92 @@ public class MainActivity extends AppCompatActivity {
         imageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(MainActivity.this, DeskripsiBeranda2.class);
-                startActivity(intent2);
+                Intent intent = new Intent(MainActivity.this, DeskripsiBeranda2.class);
+                startActivity(intent);
             }
         });
         imageView3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent3 = new Intent(MainActivity.this, DeskripsiBeranda3.class);
-                startActivity(intent3);
+                Intent intent = new Intent(MainActivity.this, DeskripsiBeranda3.class);
+                startActivity(intent);
             }
         });
         imageView4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent4 = new Intent(MainActivity.this, DeskripsiBeranda4.class);
-                startActivity(intent4);
+                Intent intent = new Intent(MainActivity.this, DeskripsiBeranda4.class);
+                startActivity(intent);
             }
         });
         imageView5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent5 = new Intent(MainActivity.this, DeskripsiBeranda5.class);
-                startActivity(intent5);
+                Intent intent = new Intent(MainActivity.this, DeskripsiBeranda5.class);
+                startActivity(intent);
+            }
+        });
+        imageView6.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, DeskripsiBeranda6.class);
+                startActivity(intent);
+            }
+        });
+        imageView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DeskripsiBeranda7.class);
+                startActivity(intent);
+            }
+        });
+        imageView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DeskripsiBeranda8.class);
+                startActivity(intent);
+            }
+        });
+        imageView9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DeskripsiBeranda9.class);
+                startActivity(intent);
+            }
+        });
+        imageView10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DeskripsiBeranda10.class);
+                startActivity(intent);
+            }
+        });
+        imageView11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DeskripsiBeranda11.class);
+                startActivity(intent);
+            }
+        });
+        imageView12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Syarat_dan_Ketentuan.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Syarat_dan_Ketentuan.class);
+                startActivity(intent);
             }
         });
     }
