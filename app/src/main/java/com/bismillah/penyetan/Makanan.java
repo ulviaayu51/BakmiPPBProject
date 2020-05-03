f2d3a1304023a14cc568d3782cb5245a9f40b92f
ulviaayuc <47856128+ulviaayu51@users.noreply.github.com>
2020-05-03 21:37:12 +0700

    Update Makanan.java

--- app/src/main/java/com/bismillah/penyetan/Makanan.java
+++ app/src/main/java/com/bismillah/penyetan/Makanan.java
@@ -16,12 +16,30 @@ public class Makanan extends AppCompatActivity {
     ImageView imageView4;
     ImageView imageView5;
     ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    ImageView imageView10;
+    ImageView imageView11;
+    ImageView imageView12;
+    ImageView imageView13;
+    ImageView imageView14;
+    ImageView imageView15;
     TextView textView1;
     TextView textView2;
     TextView textView3;
     TextView textView4;
     TextView textView5;
     TextView textView6;
+    TextView textView7;
+    TextView textView8;
+    TextView textView9;
+    TextView textView10;
+    TextView textView11;
+    TextView textView12;
+    TextView textView13;
+    TextView textView14;
+    TextView textView15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +49,7 @@ public class Makanan extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         TextView toolbarText = findViewById(R.id.textView_toolbar);
         if(toolbarText!=null && toolbar!=null){
-            toolbarText.setText("Makanan");
+            toolbarText.setText("Bakmi");
             setSupportActionBar(toolbar);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
@@ -42,12 +60,31 @@ public class Makanan extends AppCompatActivity {
         imageView4 = findViewById(R.id.imageView4_makanan);
         imageView5 = findViewById(R.id.imageView5_makanan);
         imageView6 = findViewById(R.id.imageView6_makanan);
+        imageView7 = findViewById(R.id.imageView7_makanan);
+        imageView8 = findViewById(R.id.imageView8_makanan);
+        imageView9 = findViewById(R.id.imageView9_makanan);
+        imageView10 = findViewById(R.id.imageView10_makanan);
+        imageView11 = findViewById(R.id.imageView11_makanan);
+        imageView12 = findViewById(R.id.imageView12_makanan);
+        imageView13 = findViewById(R.id.imageView13_makanan);
+        imageView14 = findViewById(R.id.imageView14_makanan);
+        imageView15 = findViewById(R.id.imageView15_makanan);
         textView1 = findViewById(R.id.textView1_makanan);
         textView2 = findViewById(R.id.textView2_makanan);
         textView3 = findViewById(R.id.textView3_makanan);
         textView4 = findViewById(R.id.textView4_makanan);
         textView5 = findViewById(R.id.textView5_makanan);
         textView6 = findViewById(R.id.textView6_makanan);
+        textView7 = findViewById(R.id.textView7_makanan);
+        textView8 = findViewById(R.id.textView8_makanan);
+        textView9 = findViewById(R.id.textView9_makanan);
+        textView10 = findViewById(R.id.textView10_makanan);
+        textView11 = findViewById(R.id.textView11_makanan);
+        textView12 = findViewById(R.id.textView12_makanan);
+        textView13 = findViewById(R.id.textView13_makanan);
+        textView14 = findViewById(R.id.textView14_makanan);
+        textView15 = findViewById(R.id.textView15_makanan);
+
 
         imageView1.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -59,36 +96,99 @@ public class Makanan extends AppCompatActivity {
         imageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(Makanan.this, DeskripsiMakanan2.class);
-                startActivity(intent2);
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan2.class);
+                startActivity(intent);
             }
         });
-        imageView3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent3 = new Intent(Makanan.this, DeskripsiMakanan3.class);
-                startActivity(intent3);
-            }
-        });
-        imageView4.setOnClickListener(new View.OnClickListener(){
+        imageView3.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent4 = new Intent(Makanan.this, DeskripsiMakanan4.class);
-                startActivity(intent4);
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan7.class);
+                startActivity(intent);
             }
         });
-        imageView5.setOnClickListener(new View.OnClickListener() {
+        imageView4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent5 = new Intent(Makanan.this, DeskripsiMakanan5.class);
-                startActivity(intent5);
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan8.class);
+                startActivity(intent);
             }
         });
-        imageView6.setOnClickListener(new View.OnClickListener() {
+        imageView5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan9.class);
+                startActivity(intent);
+            }
+        });
+        imageView6.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan10.class);
+                startActivity(intent);
+            }
+        });
+        imageView7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent6 = new Intent(Makanan.this, DeskripsiMakanan6.class);
-                startActivity(intent6);
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan11.class);
+                startActivity(intent);
+            }
+        });
+        imageView8.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan12.class);
+                startActivity(intent);
+            }
+        });
+        imageView9.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan13.class);
+                startActivity(intent);
+            }
+        });
+        imageView10.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan25.class);
+                startActivity(intent);
+            }
+        });
+        imageView11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan26.class);
+                startActivity(intent);
+            }
+        });
+        imageView12.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan27.class);
+                startActivity(intent);
+            }
+        });
+        imageView13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan28.class);
+                startActivity(intent);
+            }
+        });
+        imageView14.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan29.class);
+                startActivity(intent);
+            }
+        });
+        imageView15.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Makanan.this, DeskripsiMakanan30.class);
+                startActivity(intent);
             }
         });
     }
