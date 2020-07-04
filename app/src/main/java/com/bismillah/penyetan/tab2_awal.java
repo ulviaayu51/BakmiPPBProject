29d23bcbd6f0fe06bb3c4f6ed838eaf1abb07711
munawaroh04 <45156930+munawaroh04@users.noreply.github.com>
2020-07-04 13:11:33 +0700

    tab2_awal.java

---
+++ app/src/main/java/com/bismillah/penyetan/tab2_awal.java
@@ -0,0 +1,24 @@
+package com.bismillah.penyetan;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class tab2_awal extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView( LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.tab2_awal,container,false);
+
+
+
+
+        return view;
+
+    }
+}
