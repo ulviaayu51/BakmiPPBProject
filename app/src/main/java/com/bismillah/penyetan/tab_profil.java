4894f808b8168bf10e49875bad3cd0e0077ea0ea
munawaroh04 <45156930+munawaroh04@users.noreply.github.com>
2020-07-04 13:13:13 +0700

    tab_profil.java

---
+++ app/src/main/java/com/bismillah/penyetan/tab_profil.java
@@ -0,0 +1,21 @@
+package com.bismillah.penyetan;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class tab_profil extends Fragment {
+    public tab_profil() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_profil, container, false);
+    }
+}
